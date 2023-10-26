0ffcdb775280e79aeeeb82a1cdb11c3bf4c3f09b
Julianlavalla <149015515+Julianlavalla@users.noreply.github.com>
2023-10-26 15:44:38 -0300

    Delete act 3 directory

--- act 3/script.js
+++
@@ -1,8 +0,0 @@
-function metrosPies () {
-
-    var metros = document.getElementById("metros").value;
-
-    var metrosPorPies = metros * 3.28;
-
-    document.getElementById("metrosPies").innerText = metrosPorPies;
-}
